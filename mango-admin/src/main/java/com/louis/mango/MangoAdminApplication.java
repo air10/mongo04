1b2cceaa806cd033c6bcde757f8bfbc2e455adf3
hanlei <14816856@qq.com>
2024-11-25 01:10:17 +0800

    主类改了个名字

--- mango-admin/src/main/java/com/louis/mango/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/louis/mango/MangoAdminApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
-public class MangoApplication {
+public class MangoAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MangoApplication.class, args);
+        SpringApplication.run(MangoAdminApplication.class, args);
     }
 
 }
