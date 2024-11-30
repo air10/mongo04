d5b9ff4b9dffcf8491369a66f91537ef02d543f7
hanlei <14816856@qq.com>
2024-11-30 13:53:43 +0800

    13 章 系统服务监控 完成

---
+++ mango-monitor/src/test/java/com/louis/mango/monitor/MangoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.louis.mango.monitor;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MangoMonitorApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
