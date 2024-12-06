126d7cb8ee39d1482ed1a67e1564f52156838cd4
hanlei <14816856@qq.com>
2024-12-06 19:34:19 +0800

    15.3.10 ribbon 负载均衡完成。 最后改配置文件，轮询改随机，不弄了。

--- mango-consumer/src/main/java/com/louis/mango/consumer/MangoConsumerApplication.java
+++ mango-consumer/src/main/java/com/louis/mango/consumer/MangoConsumerApplication.java
@@ -3,6 +3,9 @@ package com.louis.mango.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class MangoConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MangoConsumerApplication.class, args);
+    }
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
