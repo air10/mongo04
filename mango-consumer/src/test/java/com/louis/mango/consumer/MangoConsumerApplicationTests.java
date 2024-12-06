6f5b69fa4431cdba5acf3e24b787c67040e8768d
hanlei <14816856@qq.com>
2024-12-06 19:14:16 +0800

    第十三章  服务消费 15.3.5完成 ，RestTemplate +LoadBalancer 实现了消费服务和负载均衡（轮询）

---
+++ mango-consumer/src/test/java/com/louis/mango/consumer/MangoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.louis.mango.consumer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MangoConsumerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
