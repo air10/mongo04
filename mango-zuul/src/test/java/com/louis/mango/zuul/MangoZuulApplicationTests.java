d748980b5c1db0cccd28392ffa9a44bebd72b1c3
hanlei <14816856@qq.com>
2024-12-07 22:09:21 +0800

    第十七章 结束 17.48 17.49，不弄了。没啥用

---
+++ mango-zuul/src/test/java/com/louis/mango/zuul/MangoZuulApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.louis.mango.zuul;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MangoZuulApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
