7ed26c740af04c35a2dbd7a6880ab162a267a1fe
hanlei <14816856@qq.com>
2024-12-07 20:23:55 +0800

    第16章节，服务熔断完成。

---
+++ mango-hystrix/src/test/java/com/louis/mango/hystrix/MangoHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.louis.mango.hystrix;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MangoHystrixApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
